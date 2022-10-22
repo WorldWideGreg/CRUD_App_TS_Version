5b6bb359591e10a3f61002bfcba9fee65cf9f8c9
WorldWideGreg <greg_hehe@yahoo.fr>
2022-10-22 20:27:03 +0200

    push 0

---
+++ components/addUserForm.tsx
@@ -0,0 +1,115 @@
+import { useReducer } from "react";
+import { BiPlusCircle } from "react-icons/bi";
+import Success from "./success";
+
+const formReducer = (state:any, event:any) => {
+  return {
+    ...state,
+    [event.target.name]: event.target.value,
+  };
+};
+
+function AddUserForm() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const handleSubmit = (event:any) => {
+    event.preventDefault();
+    console.log(JSON.stringify(formData));
+  };
+
+  if (Object.keys(formData).length > 0) {
+    return <Success message={"New Teamate Added"} />;
+  }
+
+  return (
+    <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>
+      <div className="input-type">
+        <input
+          type="text"
+          name="firstname"
+          onChange={setFormData}
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="FirstName"
+          required
+        />
+      </div>
+      <div className="input-type">
+        <input
+          type="text"
+          onChange={setFormData}
+          name="lastname"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="LastName"
+          required
+        />
+      </div>
+      <div className="input-type">
+        <input
+          type="text"
+          onChange={setFormData}
+          name="email"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="E-mail"
+          required
+        />
+      </div>
+      <div className="input-type">
+        <input
+          type="text"
+          onChange={setFormData}
+          name="salary"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Salary"
+        />
+      </div>
+      <div className="input-type">
+        <input
+          type="date"
+          onChange={setFormData}
+          name="date"
+          className="border px-5 py-3 focus:outline-none rounded-md"
+          placeholder="birthday"
+          required
+        />
+      </div>
+
+      <div className="flex gap-10 items-center">
+        <div className="form-check">
+          <input
+            type="radio"
+            required
+            onChange={setFormData}
+            name="status"
+            value="Active"
+            id="radioDefault1"
+            className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-green-500 checked:border-gray-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+          />
+          <label htmlFor="radioDefault1" className="inline-block text-gray-800">
+            Active
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            type="radio"
+            required
+            onChange={setFormData}
+            name="status"
+            value="Inactive"
+            id="radioDefault2"
+            className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-red-500 checked:border-gray-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+          />
+          <label htmlFor="radioDefault2" className="inline-block text-gray-800">
+            Inactive
+          </label>
+        </div>
+      </div>
+      <button className="flex justify-center text-md w-1/4 bg-blue-500 text-white px-4 py-2 border rounded-md hover:bg-blue-200 hover:border-blue-600 hover:text-gray-800">
+        Add{" "}
+        <span className="px-1">
+          {" "}
+          <BiPlusCircle size={22}></BiPlusCircle>{" "}
+        </span>
+      </button>
+    </form>
+  );
+}
+export default AddUserForm;
---
+++ components/table.tsx
@@ -0,0 +1,75 @@
+import { BiEdit, BiTrashAlt } from "react-icons/bi";
+import data from "../database/data.json";
+
+function Table() {
+  return (
+    <table className="min-w-full table-auto">
+      <thead>
+        <tr className="bg-gray-800">
+          <th className="px-15 py-2">
+            <span className="text-gray-200">Name</span>
+          </th>
+          <th className="px-15 py-2">
+            <span className="text-gray-200">E-mail</span>
+          </th>
+          <th className="px-15 py-2">
+            <span className="text-gray-200">Salary</span>
+          </th>
+          <th className="px-15 py-2">
+            <span className="text-gray-200">Birthday</span>
+          </th>
+          <th className="px-15 py-2">
+            <span className="text-gray-200">Status</span>
+          </th>
+          <th className="px-15 py-2">
+            <span className="text-gray-200">Actions</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody className="bg-gray-200">
+        {
+          data.map((obj,i)=> <Tr {...obj} key={i} />)
+         
+        }
+        
+
+      </tbody>
+    </table>
+  );
+}
+
+function Tr({id,name,avatar,email,salary,date,status}:any) {
+  return (
+    <tr className="bg-gray-50 text-center">
+      <td className="px-16 py-2 flex flex-row items-center">
+        <img src={avatar||"#"} alt="" />
+        <span className="text-center ml-2 font-semibold">{name||"Unknown"}</span>
+      </td>
+      <td className="px-16 py-2">
+        <span>{email||"Unknown"}</span>
+      </td>
+      <td className="px-16 py-2">
+        <span>{salary||"Unknown"}</span>
+      </td>
+      <td className="px-16 py-2">
+        <span>{date||"Unknown"}</span>
+      </td>
+      <td className="px-16 py-2">
+        <button className="cursor">
+          <span className="bg-green-600 text-white px-5 py-1 rounded-full">
+            {status}
+          </span>
+        </button>
+      </td>
+      <td className="px-16 py-2 flex justify-around gap-5">
+        <button className="cursor">
+          <BiEdit size={25} color={"blue"}></BiEdit>
+        </button>
+        <button className="cursor">
+          <BiTrashAlt size={25} color={"red"}></BiTrashAlt>
+        </button>
+      </td>
+    </tr>
+  );
+}
+export default Table;
